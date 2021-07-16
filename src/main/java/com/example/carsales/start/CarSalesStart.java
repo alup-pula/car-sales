a27b6f6c9f5220af8f0d4d4fbe651cda9a7abf0b
罗业伦 <luoyelun@foxmail.com>
2021-07-16 16:14:01 +0800

    update

--- src/main/java/com/example/carsales/start/CarSalesStart.java
+++ src/main/java/com/example/carsales/start/CarSalesStart.java
@@ -3,32 +3,27 @@ package com.example.carsales.start;
 import com.example.carsales.dao.UserDao;
 import com.example.carsales.entity.User;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.ApplicationListener;
-import org.springframework.context.event.ContextRefreshedEvent;
+import org.springframework.boot.ApplicationArguments;
+import org.springframework.boot.ApplicationRunner;
 import org.springframework.stereotype.Component;
 
 /**
  * 项目启动完成后执行
  */
 @Component
-public class CarSalesStart implements ApplicationListener<ContextRefreshedEvent> {
+public class CarSalesStart implements ApplicationRunner {
     @Autowired
     UserDao userDao;
 
-    public void init() {
-        User admin = userDao.getOne(1L);
-        if (admin == null) {
-            admin = new User();
+    @Override
+    public void run(ApplicationArguments args) throws Exception {
+        if (!userDao.findById(1L).isPresent()) {
+            User admin = new User();
             admin.setId(1L);
             admin.setName("管理员");
             admin.setUsername("admin");
             admin.setPassword("admin");
             userDao.save(admin);
         }
-    }
-
-    @Override
-    public void onApplicationEvent(ContextRefreshedEvent contextRefreshedEvent) {
-        init();
     }
 }
